ff2886b7fd5901eb5990d0c382a448b009bf2864
nushey <zeballosnahuelfd@gmail.com>
2025-06-06 17:41:41 -0300

    upd adts

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -6,16 +6,22 @@
 
 using namespace std;
 
-struct Coord
+struct Figure
 {
     int x;
     int y;
     int h;
 };
 
-vector<pair<int, int>> merge(Coord &a, Coord &b)
+struct Point
 {
-    vector<pair<int, int>> ans;
+    int x;
+    int y;
+};
+
+vector<Point> merge(Figure &a, Figure &b)
+{
+    vector<Point> ans;
     if (b.x > a.y)
     {
         ans.push({a.x, a.h});
@@ -120,10 +126,10 @@ vector<pair<int, int>> merge(Coord &a, Coord &b)
         }
     }
 
-    vector<pair<int, int>> cleanAns;
+    vector<Point> cleanAns;
     for (int i = 0; i < ans.size(); i++)
     {
-        if (i == 0 || ans[i].second != ans[i - 1].second)
+        if (i == 0 || ans[i].y != ans[i - 1].y)
         {
             cleanAns.push(ans[i]);
         }
@@ -131,31 +137,31 @@ vector<pair<int, int>> merge(Coord &a, Coord &b)
     return cleanAns;
 }
 
-vector<pair<int, int>> concat(vector<pair<int, int>> &a, vector<pair<int, int>> &b)
+vector<Point> concat(vector<Point> &a, vector<Point> &b)
 {
-    vector<pair<int, int>> ans;
+    vector<Point> ans;
     int i = 0, j = 0, h1 = 0, h2 = 0;
     while (i < a.size() && j < b.size())
     {
-        if (a[i].first < b[j].first)
+        if (a[i].x < b[j].x)
         {
-            int x = a[i].first;
-            h1 = a[i].second;
+            int x = a[i].x;
+            h1 = a[i].y;
             ans.push({x, max(h1, h2)});
             i++;
         }
-        else if (a[i].first > b[j].first)
+        else if (a[i].x > b[j].x)
         {
-            int x = b[j].first;
-            h2 = b[j].second;
+            int x = b[j].x;
+            h2 = b[j].y;
             ans.push({x, max(h1, h2)});
             j++;
         }
         else
         {
-            int x = a[i].first;
-            h1 = a[i].second;
-            h2 = b[j].second;
+            int x = a[i].x;
+            h1 = a[i].y;
+            h2 = b[j].y;
             ans.push({x, max(h1, h2)});
             i++;
             j++;
@@ -170,10 +176,10 @@ vector<pair<int, int>> concat(vector<pair<int, int>> &a, vector<pair<int, int>> 
         ans.push(b[j++]);
     }
 
-    vector<pair<int, int>> cleanAns;
+    vector<Point> cleanAns;
     for (int i = 0; i < ans.size(); i++)
     {
-        if (i == 0 || ans[i].second != ans[i - 1].second)
+        if (i == 0 || ans[i].y != ans[i - 1].y)
         {
             cleanAns.push(ans[i]);
         }
@@ -181,11 +187,11 @@ vector<pair<int, int>> concat(vector<pair<int, int>> &a, vector<pair<int, int>> 
     return cleanAns;
 }
 
-vector<pair<int, int>> solve(int n, Coord *arr, int l, int r)
+vector<Point> solve(int n, Figure *arr, int l, int r)
 {
     if (r == l)
     {
-        vector<pair<int, int>> result;
+        vector<Point> result;
         result.push({arr[l].x, arr[l].h});
         result.push({arr[l].y, 0});
         return result;
@@ -195,8 +201,8 @@ vector<pair<int, int>> solve(int n, Coord *arr, int l, int r)
         return merge(arr[l], arr[r]);
     }
     int mid = (l + r) / 2;
-    vector<pair<int, int>> left = solve(n, arr, l, mid);
-    vector<pair<int, int>> right = solve(n, arr, mid + 1, r);
+    vector<Point> left = solve(n, arr, l, mid);
+    vector<Point> right = solve(n, arr, mid + 1, r);
     return concat(left, right);
 }
 
@@ -204,21 +210,21 @@ int main()
 {
     int n;
     cin >> n;
-    Coord *arr = new Coord[n];
+    Figure *arr = new Figure[n];
     for (int i = 0; i < n; i++)
     {
         int x, y, h;
         cin >> x >> y >> h;
         arr[i] = {x, y, h};
     }
-    vector<pair<int, int>> ans = solve(n, arr, 0, n - 1);
+    vector<Point> ans = solve(n, arr, 0, n - 1);
     for (int i = 0; i < ans.size(); i++)
     {
         if (i != 0)
         {
             cout << "\n";
         }
-        cout << ans[i].first << " " << ans[i].second;
+        cout << ans[i].x << " " << ans[i].y;
     }
     cout << "\n";
 }
